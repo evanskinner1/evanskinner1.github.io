7348a3238337f3d503e34704da89d5e20b4c7324
evanskinner1 <ers8000@gmail.com>
2020-05-15 13:39:27 +1400

    mod 2 lastmodified

--- lesson4/js/currentdate.js
+++ lesson4/js/currentdate.js
@@ -1,19 +1,17 @@
+
 let date = new Date(document.lastModified);
-var format = "YYYY-MMM-DD DDD";
-document.write(dateConvert(date,format));
+document.write(dateConvert(date));
 
 function dateConvert(dateobj){
   var year = dateobj.getFullYear();
-  var month= ("0" + (dateobj.getMonth()+1)).slice(-2);
-  var date = ("0" + dateobj.getDate()).slice(-2);
+  var month= dateobj.getMonth();
+  var date = dateobj.getDate();
   var day = dateobj.getDay();
   var months = ["January","February","March","April","May","June","Jul","August","September","October","November","December"];
   var dates = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
 
   return dates[parseInt(day)] + " " + date + " " + months[parseInt(month)-1] + " " + year;
 }
-
-
 /*var string = document.lastModified;
 
 document.write("Last Updated:" + " " + document.lastModified);*/
