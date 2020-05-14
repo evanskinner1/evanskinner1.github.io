51429512d7c6c4c3bb14eb0bb20e9cd5aa8f7057
evanskinner1 <ers8000@gmail.com>
2020-05-14 12:24:24 -1100

    Date formatting and troubleshoot

--- lesson4/js/currentdate.js
+++ lesson4/js/currentdate.js
@@ -1,10 +1,17 @@
+let date = new Date(document.lastModified);
+var format = "YYYY-MMM-DD DDD";
+document.write(dateConvert(date,format));
 
-var now = new Date();
-var month = ['January','February','March','April','May','June','July','August','September','October','November','December'];
-var day = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
-var thedate = day[now.getDay()]+", " + month[now.getMonth()] +" "+ now.getDate() + ", " + now.getFullYear();
-document.getElementById("currentdate").innerHTML=thedate;
+function dateConvert(dateobj){
+  var year = dateobj.getFullYear();
+  var month= ("0" + (dateobj.getMonth()+1)).slice(-2);
+  var date = ("0" + dateobj.getDate()).slice(-2);
+  var day = dateobj.getDay();
+  var months = ["January","February","March","April","May","June","Jul","August","September","October","November","December"];
+  var dates = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
 
+  return dates[parseInt(day)] + " " + date + " " + months[parseInt(month)-1] + " " + year;
+}
 
 
 /*var string = document.lastModified;
