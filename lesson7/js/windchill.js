6f6e55ae7792ce57f98a413634d0f866790b3b68
evanskinner1 <ers8000@gmail.com>
2020-05-27 14:55:00 -0600

    lesson6 Assignment

---
+++ lesson7/js/windchill.js
@@ -0,0 +1,32 @@
+document.addEventListener("DOMContentLoaded", function () {
+
+
+    let temp = parseFloat(document.getElementById("ctemp").textContent);
+    let speed = parseFloat(document.getElementById("cspeed").textContent);
+
+    let windchill = document.getElementById('windchill');
+
+    windchill.innerHTML = buildWC(speed, temp);
+});
+
+// Calculate the Windchill
+function buildWC(speed, temp) {
+
+
+    //let windchill = document.getElementById('windchill');
+
+    // Compute the windchill
+    let wc = 35.74 + 0.6215 * temp - 35.75 * Math.pow(speed, 0.16) + 0.4275 * temp * Math.pow(speed, 0.16);
+    console.log(wc);
+
+    // Round the answer down to integer
+    wc = Math.floor(wc);
+
+    // If chill is greater than temp, return the temp
+    wc = (wc > temp) ? temp : wc;
+
+    // Display the windchill
+    console.log(wc);
+    //windchill.innerHTML = wc;
+    return wc;
+}
