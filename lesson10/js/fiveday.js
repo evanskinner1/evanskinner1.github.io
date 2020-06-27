72d49e1d73f11e144c3614c8649b2db0c5a25192
evanskinner1 <ers8000@gmail.com>
2020-06-27 10:32:38 -0600

    change request to https

--- lesson10/js/fiveday.js
+++ lesson10/js/fiveday.js
@@ -1,5 +1,5 @@
 
-fetch('http://api.openweathermap.org/data/2.5/forecast?id=5604473&APPID=96e53194a6fddf94252efa4fc674c5d6&units=imperial')
+fetch('https://api.openweathermap.org/data/2.5/forecast?id=5604473&APPID=96e53194a6fddf94252efa4fc674c5d6&units=imperial')
     .then((response) => response.json())
     .then((forecastData) => {
        
